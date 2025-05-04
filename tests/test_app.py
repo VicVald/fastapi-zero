1928337db156d47a5c88c7b65f510e1cb8301073
Victor <victorhugokawano@gmail.com>
2025-05-04 12:40:31 -0300

    adding gitignore

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,23 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from project_1.app import app
 
 client = TestClient(app)
+
+def test_root_deve_retornar_ok_e_ola_mundo():  
+
+
+    client = TestClient(app)  
+
+
+
+    response = client.get('/')  
+
+
+
+    assert response.status_code == HTTPStatus.OK  
+
+
+    assert response.json() == {'message': 'Olá Mundo!'} 
